36001533384b1205a41119ec28843b115863cbb8
Romulop90 <144858155+Romulop90@users.noreply.github.com>
2024-03-11 20:48:55 -0300

    2 aula de jaVA

--- B7Web/Projetos Java/JavaScript/script.js
+++ B7Web/Projetos Java/JavaScript/script.js
@@ -1,9 +1,25 @@
-let idade = 90
-let nome = "Bonieky"
-let cidade = "belém"
-let estado = "Para"
+let nomeDoCarro = "Ferrari"
+let valordoBolo = 19.99
+let cidade = "são Paulo"
 
-console.log("nome")
-console.log("idade")
-console.log("cidade")
-console.log("estado")
+console.log(nomeDoCarro)
+console.log(19.99)
+console.log(cidade)
+
+let nome = "Bonieky"
+let idade = 90
+
+console.log(nome)
+console.log(idade)
+
+let jogo = "Super Mario";
+let manga = "spy x Family"
+
+console.log(jogo)
+console.log(manga)
+
+let x = 0;
+
+x = x / 8
+
+console.log(x)
