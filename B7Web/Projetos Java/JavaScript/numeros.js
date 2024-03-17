2281d5335ade81ec4ddbd68d4faa7f6d7530afe8
Romulop90 <144858155+Romulop90@users.noreply.github.com>
2024-03-17 19:21:41 -0300

    exercicios

---
+++ B7Web/Projetos Java/JavaScript/numeros.js
@@ -0,0 +1,9 @@
+let idade = 30;
+
+if (idade < 18) {
+  console.log("voce e uma crianca")
+} else if (idade >= 18 && idade < 60) {
+  console.log("voce e uma aduto")
+} else if (idade >= 60) {
+  console.log("voce e uma idoso")
+}
