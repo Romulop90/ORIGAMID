7473383d9abe058b836e09c5a61228d2d695903d
Romulop90 <144858155+Romulop90@users.noreply.github.com>
2024-03-31 19:24:29 -0300

    Update javaScripit03.js

--- B7Web/CrusoJs/aula004/exer006/javaScripit03.js
+++ B7Web/CrusoJs/aula004/exer006/javaScripit03.js
@@ -1,8 +1,8 @@
-var n1 = Number(window.prompt('Digite um Numero: '));
-var n2 = Number(window.prompt('Digite outro Numero: '));
-var s = n1 + n2;
+// var n1 = Number(window.prompt('Digite um Numero: '));
+// var n2 = Number(window.prompt('Digite outro Numero: '));
+// var s = n1 + n2;
 // window.alert('A soma dos valores é' + s)
-window.alert(`A soma entre ${n1} e ${n2} e igual a ${s}`)
+//window.alert(`A soma entre ${n1} e ${n2} e igual a ${s}`)
 // (number + numeber) + para adição
 // (string + string) + para concatenação
 
@@ -12,3 +12,24 @@ formatação String
 'eu estou aprendendo' + s usa concatenaçao
 `eu estou aprendendo ${s}` usa template string
 */
+
+// let nome = "boniek";
+// let sobrenome = "Larcerda";
+// //let nomeCompleto = nome + " " + sobrenome;
+// let nomeCompleto = `${nome} ${sobrenome}`;
+// console.log("nome: " + nomeCompleto);
+
+let idade = 90;
+
+if (idade > 17) {
+  console.log("voce e maior de idade.");
+} else {
+  console.log("voce Menor de idade.");
+}
+
+// >
+// <
+// ==
+// >=
+// <=
+// !=
