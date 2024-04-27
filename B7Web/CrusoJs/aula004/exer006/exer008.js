13043e632e8c60f18c3ae0ea78010084615b6c93
Romulop90 <144858155+Romulop90@users.noreply.github.com>
2024-04-27 20:01:22 -0300

    exer

---
+++ B7Web/CrusoJs/aula004/exer006/exer008.js
@@ -0,0 +1,6 @@
+var vel = 72;
+console.log(`A velocidade do seu carro e ${vel}km/h`);
+if (vel > 60) { // Condição simples
+  console.log(`Voce ultrapassou a velocidade permitida. Multado`);
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
---
+++ B7Web/CrusoJs/aula004/exer006/exer009.js
@@ -0,0 +1,7 @@
+var pais = "USA";
+console.log(`Vive em ${pais}`);
+if (pais == "Brasil") {
+  console.log("Voce e Brasileiro!");
+} else {
+  console.log("Voce e Estrangeiro!");
+}
